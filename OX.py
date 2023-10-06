8f54e3b83e37a769c1a8eeb03dd27cc566b44a3e
hedgelog <hedgeloggy@gmail.com>
2023-10-06 14:35:20 +0300

    rename GameField in Board

--- OX.py
+++ OX.py
@@ -1,22 +1,16 @@
 from enum import Enum
-
-class GameField:
+class Board:
     def __init__(self):
         self.height = 3
         self.width = 3
         self.cells = [[Cell.VOID] * self.width for i in range(self.height)]
-
-
 class Cell(Enum):
     VOID = 0
     CROSS = 1
     ZERO = 2
-
 class Player:
     def __init__(self, cell_type):
         self.cell_type = cell_type
-
-
 class GameManager:
     """
     Starts all processes.
@@ -24,4 +18,4 @@ class GameManager:
     def __init__(self, player1: Player, player2: Player):
         self._players = [player1, player2]  # почему приватный ??
         self.current_player = 0
-        self.field = GameField()
+        self.board = Board()
