ee86df1af819f2452549f35097482a328b7f2bab
hedgelog <hedgeloggy@gmail.com>
2023-10-06 14:36:56 +0300

    Dentins

--- OX.py
+++ OX.py
@@ -1,16 +1,24 @@
 from enum import Enum
+
+
 class Board:
     def __init__(self):
         self.height = 3
         self.width = 3
         self.cells = [[Cell.VOID] * self.width for i in range(self.height)]
+
+
 class Cell(Enum):
     VOID = 0
     CROSS = 1
     ZERO = 2
-class Player:
+
+
+    class Player:
     def __init__(self, cell_type):
         self.cell_type = cell_type
+
+        
 class GameManager:
     """
     Starts all processes.
