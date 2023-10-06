7f46515a25f797e67236b6beb86244a1342cd94b
hedgelog <hedgeloggy@gmail.com>
2023-10-06 14:31:04 +0300

    Created Player, GameManager

--- OX.py
+++ OX.py
@@ -1,4 +1,6 @@
-class Board:
+from enum import Enum
+
+class GameField:
     def __init__(self):
         self.height = 3
         self.width = 3
@@ -9,3 +11,17 @@ class Cell(Enum):
     VOID = 0
     CROSS = 1
     ZERO = 2
+
+class Player:
+    def __init__(self, cell_type):
+        self.cell_type = cell_type
+
+
+class GameManager:
+    """
+    Starts all processes.
+    """
+    def __init__(self, player1: Player, player2: Player):
+        self._players = [player1, player2]  # почему приватный ??
+        self.current_player = 0
+        self.field = GameField()
