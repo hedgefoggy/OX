907eb44ad64b65288574218b88e6776079d0f9c6
hedgelog <hedgeloggy@gmail.com>
2023-10-12 07:41:13 +0300

    class Player + class Board

--- OX.py
+++ OX.py
@@ -1,32 +1,13 @@
-from enum import Enum
-
+#from enum import Enum
 
 class Board:
-    def __init__(self):
-        self.cells = [[Cell.VOID,Cell.VOID, Cell.VOID],
-                      [Cell.VOID,Cell.VOID, Cell.VOID],
-                      [Cell.VOID,Cell.VOID, Cell.VOID]]
-
-
-class Cell(Enum):
-    VOID = 0
-    CROSS = 1
-    ZERO = 2
-
+    cells = [[None, None, None],
+             [None, None, None],
+             [None, None, None]]
+    def insert (self, x, y, player):
+        if cells[x, y] == None:
+            cells[x,y] = player  
 
 class Player:
-    def __init__(self, player,cell_type):
-        self.cell_type = Cell(Enum)
-        self.player = player
-
-    def get_icon (self):
-        return  player.cell_type
-
-class Game:
-    def __init__(self, player1: Player, player2: Player):
-        self.player1 = Player()
-        self.player1 = Player()
-
-        self.current_player = 0
-        self.board = Board()
-
+    def turn(self, board, x, y):
+        board.insert(x,y)
