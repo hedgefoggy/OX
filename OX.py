2d4aff5cdc1962f97b5f67701c988798426b92df
hedgelog <hedgeloggy@gmail.com>
2023-10-14 13:49:38 +0300

    startup attempt 2

--- OX.py
+++ OX.py
@@ -1,7 +1,8 @@
 #from enum import Enum
 
 class Board:
-    cells = [[None, None, None],
+    def __init__(self, cells):
+        self.cells = [[None, None, None],
              [None, None, None],
              [None, None, None]]
     def insert (self, x, y, player):
@@ -16,6 +17,5 @@ player1 = Player()
 player2 = Player()
 board = Board()
 
-player1.turn(board.insert(x = 1, y=1,player1), x=1, y=1)
 
-print(Board.cells)
+print(board.cells)
