050e18266df52becd0bc2babb29b1c7471484981
hedgelog <hedgeloggy@gmail.com>
2023-10-14 13:31:51 +0300

    startup attempt 1

--- OX.py
+++ OX.py
@@ -11,3 +11,11 @@ class Board:
 class Player:
     def turn(self, board, x, y):
         board.insert(x,y)
+
+player1 = Player()
+player2 = Player()
+board = Board()
+
+player1.turn(board.insert(x = 1, y=1,player1), x=1, y=1)
+
+print(Board.cells)
