a5f41681089910145d1f5798a9b778179367a235
hedgelog <hedgeloggy@gmail.com>
2023-10-22 20:39:19 +0300

    rules in Board()

--- OX.py
+++ OX.py
@@ -16,6 +16,17 @@ class Board:
         else:
             self.cells[x][y] = player.badge
 
+    def check_winner(self):
+        current_player = player.badge
+        if  self.cells[0][0] == current_player and self.cells[0][1] == current_player and self.cells[0][2] == current_player or \
+        self.cells[0][0] == current_player and self.cells[1][1] == current_player and self.cells[2][2] == current_player or \
+        self.cells[1][0] == current_player and self.cells[1][1] == current_player and self.cells[1][2] == current_player or \
+        self.cells[2][0] == current_player and self.cells[2][1] == current_player and self.cells[2][2] == current_player or \
+        self.cells[0][1] == current_player and self.cells[1][1] == current_player and self.cells[2][1] == current_player or \
+        self.cells[0][2] == current_player and self.cells[1][2] == current_player and self.cells[2][2] == current_player or \
+        self.cells[0][0] == current_player and self.cells[1][0] == current_player and self.cells[2][0] == current_player or \
+        self.cells[0][2] == current_player and self.cells[1][1] == current_player and self.cells[2][0] == current_player:
+            print("Winner is: ", current_player)
 
 class Player:
     badge = ""
@@ -30,22 +41,11 @@ board = Board()
 player1.badge = "X"
 player1.turn(board,0,0)
 
-player2.badge = "X"
-player2.turn(board,0,1)
+player1.badge = "X"
+player1.turn(board,0,1)
 
 player1.badge = "X"
 player1.turn(board,0,2)
 
-def check_winner(player):
-    current_player = player.badge
-    if  board.cells[0][0] == current_player and board.cells[0][1] == current_player and board.cells[0][2] == current_player or \
-        board.cells[0][0] == current_player and board.cells[1][1] == current_player and board.cells[2][2] == current_player or \
-        board.cells[1][0] == current_player and board.cells[1][1] == current_player and board.cells[1][2] == current_player or \
-        board.cells[2][0] == current_player and board.cells[2][1] == current_player and board.cells[2][2] == current_player or \
-        board.cells[0][1] == current_player and board.cells[1][1] == current_player and board.cells[2][1] == current_player or \
-        board.cells[0][2] == current_player and board.cells[1][2] == current_player and board.cells[2][2] == current_player or \
-        board.cells[0][0] == current_player and board.cells[1][0] == current_player and board.cells[2][0] == current_player or \
-        board.cells[0][2] == current_player and board.cells[1][1] == current_player and board.cells[2][0] == current_player:
-            print("Winner is: ", current_player)
-
 board.print_board()
+
