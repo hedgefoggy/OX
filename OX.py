ef9ced6b8d2652c7d36bf994a339d3173c498084
hedgelog <hedgeloggy@gmail.com>
2023-10-28 23:49:10 +0300

    last list

--- OX.py
+++ OX.py
@@ -1,28 +1,33 @@
 class Board:
-    cells = [["", "", ""],
-             ["", "", ""],
-             ["", "", ""]]
+    size = 3
+    cells = [' ' for _ in range(size*size)]
 
     def print_board(self):
-        print(id(self))
-        for cell in self.cells:
-            print(cell)
+        #print(id(self))
+        size = self.size
+        print("-------")
+        for cell in range(size):
+            print("|",end="")
+            for column in range(size):
+                print(self.cells[cell * size + column],end ="|")
+            print("\n-------")
 
-    def insert(self, player, x, y):
-        self.print_board()
+    def insert(self, player, cell):
+        self.cells[cell]
+        #self.print_board()
         print()
-        if self.cells[x][y] != "":
+        if cell != "":
             print("Cell is occupied!")
             print()
         else:
-            self.cells[x][y] = player.badge
+            cell = player.badge
 
 
 class Player:
     badge = ""
 
-    def turn(self, board, x, y):
-        board.insert(self, x, y)
+    def turn(self, board, cell):
+        board.insert(self,cell)
 
 
 player1 = Player()
@@ -32,33 +37,12 @@ board = Board()
 player1.badge = 'X'
 player2.badge = 'O'
 
-board.print_board()
-while True:
-    player1.turn(board, int(input("Player 1 select cell x: ")), int(input("select cell y: ")))
-    board.print_board()
-    player2.turn(board, int(input("Player 2 select cell x: ")), int(input("select cell y: ")))
 
-    if (
-            Board.cells[0][0] == player1.badge and Board.cells[0][1] == player1.badge and Board.cells[0][2] == player1.badge or \
-            Board.cells[0][0] == player1.badge and Board.cells[1][1] == player1.badge and Board.cells[2][2] == player1.badge or \
-            Board.cells[1][0] == player1.badge and Board.cells[1][1] == player1.badge and Board.cells[1][2] == player1.badge or \
-            Board.cells[2][0] == player1.badge and Board.cells[2][1] == player1.badge and Board.cells[2][2] == player1.badge or \
-            Board.cells[0][1] == player1.badge and Board.cells[1][1] == player1.badge and Board.cells[2][1] == player1.badge or \
-            Board.cells[0][2] == player1.badge and Board.cells[1][2] == player1.badge and Board.cells[2][2] == player1.badge or \
-            Board.cells[0][0] == player1.badge and Board.cells[1][0] == player1.badge and Board.cells[2][0] == player1.badge or \
-            Board.cells[0][2] == player1.badge and Board.cells[1][1] == player1.badge and Board.cells[2][0] == player1.badge):
-        print("Winner is: ", player1.badge)
-        break
-    elif(
-            Board.cells[0][0] == player2.badge and Board.cells[0][1] == player2.badge and Board.cells[0][2] == player2.badge or \
-            Board.cells[0][0] == player2.badge and Board.cells[1][1] == player2.badge and Board.cells[2][2] == player2.badge or \
-            Board.cells[1][0] == player2.badge and Board.cells[1][1] == player2.badge and Board.cells[1][2] == player2.badge or \
-            Board.cells[2][0] == player2.badge and Board.cells[2][1] == player2.badge and Board.cells[2][2] == player2.badge or \
-            Board.cells[0][1] == player2.badge and Board.cells[1][1] == player2.badge and Board.cells[2][1] == player2.badge or \
-            Board.cells[0][2] == player2.badge and Board.cells[1][2] == player2.badge and Board.cells[2][2] == player2.badge or \
-            Board.cells[0][0] == player2.badge and Board.cells[1][0] == player2.badge and Board.cells[2][0] == player2.badge or \
-            Board.cells[0][2] == player2.badge and Board.cells[1][1] == player2.badge and Board.cells[2][0] == player2.badge):
-        print("Winner is: ", player2.badge)
-    else:
-        print("Dead heat!")
-        break
+
+while True:
+    board.print_board()
+    player1.turn(board, int(input("X select cell: ")))
+    board.print_board()
+    player2.turn(board, int(input("O select cell: ")))
+
+
